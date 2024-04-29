e36451eb8108c54c4b8e21f120c5f0ad28f1817e
Sufferal <botnari_ciprian@mail.ru>
2024-04-29 16:34:25 +0300

    feat: add header component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
+import Header from './components/Header';
 import Navbar from './components/Navbar';
 import './css/App.css';
 
 function App() {
 
   return (
-    <div className="container">
+    <div className="app-wrapper">
       <Navbar />
+      <Header />
     </div>
   );
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import '../css/Header.css'
+
+const Header = () => {
+  return (
+    <header id="header">
+      <h2 className="header-title">
+        Learn a <span className="highlight-primary">language</span> like a native
+      </h2>
+      <button className="header-btn">
+        <a className="header-btn-link" href="#get_started">
+          Start now
+        </a>
+      </button>
+    </header>
+  )
+}
+
+export default Header

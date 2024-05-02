317c0e663097d5caea802b4000c158935e2b8eb4
Ciprian <48862508+Sufferal@users.noreply.github.com>
2024-05-02 17:50:21 +0300

    feat: add sticky navbar (#8)

--- src/components/section/Navbar.jsx
+++ src/components/section/Navbar.jsx
@@ -1,10 +1,20 @@
-import React from "react";
+import React  from "react";
 import "../../css/section/Navbar.css";
 import logo from "../../assets/img/logo.png";
 import moon from "../../assets/img/moon.png";
 import sun from "../../assets/img/sun.png";
 
 const Navbar = ({ isClicked, handleChange }) => {
+  const handleLinkClick = (e, targetId) => {
+    e.preventDefault();
+    const targetElement = document.getElementById(targetId);
+    const offsetTop = targetElement.offsetTop - 100;
+    window.scrollTo({
+      top: offsetTop,
+      behavior: "smooth"
+    });
+  };
+  
   return (
     <nav id="navbar" className="navbar">
       <div className="container navbar-wrapper">
@@ -14,10 +24,10 @@ const Navbar = ({ isClicked, handleChange }) => {
         </a>
 
         <div className="navbar-links">
-          <a className="navbar-link" href="#get_started">
+          <a className="navbar-link" href="#get_started" onClick={(e) => handleLinkClick(e, "get_started")}>
             Get started
           </a>
-          <a className="navbar-link" href="#lessons">
+          <a className="navbar-link" href="#lessons" onClick={(e) => handleLinkClick(e, "lessons")}>
             Lessons
           </a>
           <a className="navbar-link" href="#about">
